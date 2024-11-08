8aa5314f0e7cf8fe93a33bfdfaae0451041fc70a
SafiZ <tcabato@addu.edu.ph>
2024-11-08 14:31:08 +0800

    Updated to use event listeners

--- scripts/unitconv.js
+++ scripts/unitconv.js
@@ -1,14 +1,14 @@
 // ------------- Temperature Conversion ---------------------- //
-function ftoc() {
+document.getElementById("fahn").addEventListener("input", () => {
     var fahrenheit = document.getElementById("fahn").value;
-        if (fahrenheit !== "") {
-             var celsius = (fahrenheit - 32) * 5 / 9;
-            document.getElementById("cel").value = celsius.toFixed(2);
-        } else {
-            document.getElementById("cel").value = ""; 
-        }
+    if (fahrenheit !== "") {
+        var celsius = (fahrenheit - 32) * 5 / 9;
+        document.getElementById("cel").value = celsius.toFixed(2);
+    } else {
+        document.getElementById("cel").value = ""; 
     }
-function ctof() {
+});
+document.getElementById("cel").addEventListener("input", () => {
     var celsius = document.getElementById("cel").value;
         if (celsius !== "") {
             var fahrenheit = (celsius * 9 / 5) + 32;
@@ -16,9 +16,9 @@ function ctof() {
         } else {
             document.getElementById("fahn").value = "";
         }
-     }
+     });
 // ---------------- Feet to Meters ------------------------ //
-function ftom() {
+document.getElementById("ft").addEventListener("input", () => {
     var foot = document.getElementById("ft").value;
         if (foot !== "") {
             var meter = foot * 0.3048;
@@ -26,8 +26,8 @@ function ftom() {
         } else {
             document.getElementById("met").value = "";
         }
-    }
-function mtof() {
+    });
+document.getElementById("met").addEventListener("input", () => {
     var meter = document.getElementById("met").value;
         if (meter !== "") {
             var foot = meter * 3.281;
@@ -35,4 +35,4 @@ function mtof() {
         } else {
             document.getElementById("ft").value = "";
         }
-    }
+    });
